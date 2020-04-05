deb8a6d50ca5276810089e7c22a80af79edc3094
Noah Hüsser <yatekii@yatekii.ch>
2020-04-06 01:45:35 +0200

    scrollback works

--- mailman/src/app.rs
+++ mailman/src/app.rs
@@ -86,24 +86,34 @@ impl<'a> App<'a> {
     }
 
     pub fn render(&mut self) {
-        let input = &self.tabs[self.current_tab].input;
+        let input = self.tabs[self.current_tab].input.clone();
+        let has_down_channel = self.tabs[self.current_tab].has_down_channel;
+        let scroll_offset = self.tabs[self.current_tab].scroll_offset;
+        let message_num = self.tabs[self.current_tab].messages.len();
         let messages = self.tabs[self.current_tab].messages.iter().enumerate();
         let tabs = &self.tabs;
         let current_tab = self.current_tab;
+        let mut height = 0;
         self.terminal
             .draw(|mut f| {
+                let constraints = if has_down_channel {
+                    &[
+                        Constraint::Length(1),
+                        Constraint::Length(3),
+                        Constraint::Min(1),
+                        Constraint::Length(3),
+                    ][..]
+                } else {
+                    &[
+                        Constraint::Length(1),
+                        Constraint::Length(3),
+                        Constraint::Min(1),
+                    ][..]
+                };
                 let chunks = Layout::default()
                     .direction(Direction::Vertical)
                     .margin(0)
-                    .constraints(
-                        [
-                            Constraint::Length(1),
-                            Constraint::Length(3),
-                            Constraint::Min(1),
-                            Constraint::Length(3),
-                        ]
-                        .as_ref(),
-                    )
+                    .constraints(constraints)
                     .split(f.size());
 
                 let text = [Text::raw("ctrl + c: quit — F-keys: switch channels")];
@@ -123,18 +133,31 @@ impl<'a> App<'a> {
                     );
                 f.render(&mut tabs, chunks[1]);
 
-                let messages = messages.map(|(i, m)| Text::raw(format!("{}: {}", i, m)));
+                height = chunks[2].height as usize - 2;
+
+                let messages = messages
+                    .map(|(i, m)| Text::raw(format!("{}: {}", i, m)))
+                    .skip(message_num - (height + scroll_offset).min(message_num))
+                    .take(height);
                 let mut messages = List::new(messages)
                     .block(Block::default().borders(Borders::ALL).title("Messages"));
                 f.render(&mut messages, chunks[2]);
 
-                let text = [Text::raw(input)];
-                let mut input = Paragraph::new(text.iter())
-                    .style(Style::default().fg(Color::Yellow))
-                    .block(Block::default().borders(Borders::ALL).title("Input"));
-                f.render(&mut input, chunks[3]);
+                if has_down_channel {
+                    let text = [Text::raw(input.clone())];
+                    let mut input = Paragraph::new(text.iter())
+                        .style(Style::default().fg(Color::Yellow))
+                        .block(Block::default().borders(Borders::ALL).title("Input"));
+                    f.render(&mut input, chunks[3]);
+                }
             })
             .unwrap();
+
+        let message_num = self.tabs[self.current_tab].messages.len();
+        let scroll_offset = self.tabs[self.current_tab].scroll_offset;
+        if message_num < height + scroll_offset {
+            self.tabs[self.current_tab].scroll_offset = message_num - height.min(message_num);
+        }
 
         // Put the cursor back inside the input box
         let height = self.terminal.size().map(|s| s.height).unwrap_or(1);
@@ -161,10 +184,7 @@ impl<'a> App<'a> {
                     false
                 }
                 Key::Char('\n') => {
-                    // self.rtt.write(
-                    //     self.tabs[self.current_tab].number,
-                    //     self.tabs[self.current_tab].input.drain(..).collect(),
-                    // );
+                    self.push_rtt();
                     false
                 }
                 Key::Char(c) => {
@@ -173,6 +193,16 @@ impl<'a> App<'a> {
                 }
                 Key::Backspace => {
                     self.tabs[self.current_tab].input.pop();
+                    false
+                }
+                Key::PageUp => {
+                    self.tabs[self.current_tab].scroll_offset += 1;
+                    false
+                }
+                Key::PageDown => {
+                    if self.tabs[self.current_tab].scroll_offset > 0 {
+                        self.tabs[self.current_tab].scroll_offset -= 1;
+                    }
                     false
                 }
                 _ => false,
@@ -215,7 +245,10 @@ impl<'a> App<'a> {
             .find(|t| t.number == channel)
             .unwrap()
             .messages
-            .extend(split.map(|s| s.to_string()));
+            .extend(split.clone().map(|s| s.to_string()));
+        if self.tabs[self.current_tab].scroll_offset != 0 {
+            self.tabs[self.current_tab].scroll_offset += split.count() - 1;
+        }
     }
 
     /// Polls the RTT target for new data on all channels.
@@ -225,6 +258,18 @@ impl<'a> App<'a> {
             if channel == 2 {
                 self.read_rtt_channel(channel);
             }
+        }
+    }
+
+    pub fn push_rtt(&mut self) {
+        if self.tabs[self.current_tab].has_down_channel {
+            self.rtt
+                .write(
+                    self.tabs[self.current_tab].number,
+                    &self.tabs[self.current_tab].input.as_bytes(),
+                )
+                .unwrap();
+            self.tabs[self.current_tab].input.clear();
         }
     }
 }
